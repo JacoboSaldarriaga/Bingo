ebf2e6eecbeacca8178a9b5378adc0e5386addad
JacoboSaldarriaga <jacobosaldarriaga07@gmail.com>
2023-07-19 21:39:56 -0500

    Initial functionality established.

---
+++ bingo.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+  console.log( "document loaded" );
+});
+
+$( window ).on( "load", function() {
+  console.log( "window loaded" );
+});
+
+const Bingobject = {
+  // Properties.
+  cache     : "",
+  currNum   : "",
+  lastNum   : "",
+
+  // RAFFLE TIME LET'SA GO ! ! !
+  RAFFLE    : function () {
+    
+  },
+
+  // Cache functions.
+  getCache  : function () {
+    
+  },
+  setCache  : function () {
+    
+  },
+}
+
